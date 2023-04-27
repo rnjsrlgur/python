d04122ad8b42e80534d1732fee305e8e5b572061
rnjsrlgur <rlgur88@gmail.com>
2023-04-27 22:10:46 +0900

    Algorithm 1 선형 검색, 보초법

--- 6_004/algorithm1.py
+++ 6_004/algorithm1.py
@@ -1,3 +1,23 @@
+# datas = [3, 2, 5, 7, 9, 1, 0, 8, 6, 4]
+# print(f'data: {datas}')
+# print(f'data length: {len(datas)}')
+#
+# searchData = int(input('찾으려는 숫자 데이터 입력 : '))
+# searchResultIdx = -1
+#
+# n = 0
+# while True:
+#     if n == len(datas):
+#         searchResultIdx = -1
+#         break
+#     elif datas[n] == searchData:
+#         searchResultIdx = n
+#         break
+#     n += 1
+#
+# print(f'searchResultIdx: [{searchResultIdx}]')
+
+# 보초법
 datas = [3, 2, 5, 7, 9, 1, 0, 8, 6, 4]
 print(f'data: {datas}')
 print(f'data length: {len(datas)}')
@@ -5,14 +25,17 @@ print(f'data length: {len(datas)}')
 searchData = int(input('찾으려는 숫자 데이터 입력 : '))
 searchResultIdx = -1
 
+datas.append(searchData)
+
 n = 0
 while True:
-    if n == len(datas):
-        searchResultIdx = -1
+    if datas[n] == searchData:
+        if n != len(datas) - 1:
+            searchResultIdx = n
         break
-    elif datas[n] == searchData:
-        searchResultIdx = n
-        break
+
     n += 1
 
+print(f'data: {datas}')
+print(f'data length: {len(datas)}')
 print(f'searchResultIdx: [{searchResultIdx}]')
